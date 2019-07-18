40ae1a528ead225e06cca73cd6be1debfaf8f28c
Mitchell Beare <Mitchellbeare@gmail.com>
2019-07-19 09:38:59 +1000

    Intialised React App
    Built root component and rendered on page.

---
+++ warframe/components/App.js
@@ -0,0 +1,14 @@
+class App extends React.Component{
+    render(){
+        return(
+            <div className="pagecontent">
+                <h1> Hello from React to Warframe </h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
