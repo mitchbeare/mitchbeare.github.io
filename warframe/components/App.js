40ecaa5bcb9137c8626a69d254a4c9416b97dcb9
Mitchell Beare <Mitchellbeare@gmail.com>
2019-07-19 09:54:01 +1000

    Removed unnecessary div

--- warframe/components/App.js
+++ warframe/components/App.js
@@ -1,9 +1,7 @@
 class App extends React.Component{
     render(){
         return(
-            <div className="pagecontent">
                 <h1> Hello from React to Warframe </h1>
-            </div>
         );
     }
 }
