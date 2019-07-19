a2106049de197a9417f27791cfbf781880c5ae62
Mitchell Beare <Mitchellbeare@gmail.com>
2019-07-19 13:54:04 +1000

    Added task list .

--- warframe/components/App.js
+++ warframe/components/App.js
@@ -12,6 +12,15 @@ class App extends React.Component{
                     <div className="cell">
                         <DailyTasks />
                     </div>
+                    <div className="cell">
+                        <h3>Stage 1 New Player grind</h3>
+                    </div>
+                    <div className="cell-6">
+                        <ol>
+                            <li><input id="item1" type="checkbox"><label for="item1">Complete Vor's Prize Quest</label></li>
+                            <li><input id="item2" type="checkbox"><label for="item2">Complete All missions on Earth</label></li>
+                        </ol>
+                    </div>
                 </div>
             </div>
         );
