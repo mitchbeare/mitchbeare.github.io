55169478c9a5d32c82392dba9eeb5eb072acbf36
Mitchell Beare <Mitchellbeare@gmail.com>
2019-07-19 14:53:36 +1000

    Added some list items and comments

--- warframe/components/App.js
+++ warframe/components/App.js
@@ -19,7 +19,17 @@ class App extends React.Component{
                         <ol>
                             <li><input id="item1" type="checkbox" /><label for="item1">Complete Vor's Prize Quest</label></li>
                             <li><input id="item2" type="checkbox" /><label for="item2">Complete All missions on Earth</label></li>
-                            <li><input id="item3" type="checkbox" /><label for="item3">Farm Mods from Earth Spy</label></li>
+                            <li><input id="item3" type="checkbox" /><label for="item3">Farm Mods from Earth</label>
+                                <ul>
+                                <li><input id="earthitem1" type="checkbox" /><label for="earthitem1">Ivara systems blue print from Cambria. (third vault 9.09% chance)</label></li>
+                                <li><input id="earthitem2" type="checkbox" /><label for="earthitem2">Farm Vicious Frost from Cambria. (third vault 9.09% chance)</label></li>
+                                <li><input id="earthitem3" type="checkbox" /><label for="earthitem3">Farm Volcanic edge from Cambria. (third vault 9.09% chance)</label></li>
+                                <li><input id="earthitem4" type="checkbox" /><label for="earthitem4">Farm Hornet strike from Cambria. (third vault 9.09% chance)</label></li>
+                                <li><input id="earthitem5" type="checkbox" /><label for="earthitem5">Farm Serration from Cambria. (third vault 9.09% chance)</label></li>
+                                <li><input id="earthitem6" type="checkbox" /><label for="earthitem6">Farm Intensify from Everest. (third excavator 8.33% chance)</label></li>
+                                </ul>
+                            </li>
+                            <li><input id="item4" type="checkbox" /><label for="item4">Unlock and complete the Venus junction</label>
                         </ol>
                     </div>
                 </div>
