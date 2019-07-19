e096c102b30bd51ba1223706e52e50760ca7a397
Mitchell Beare <Mitchellbeare@gmail.com>
2019-07-19 12:37:22 +1000

    Added Menu back in.

--- warframe/components/App.js
+++ warframe/components/App.js
@@ -4,6 +4,7 @@ class App extends React.Component{
     render(){
         return(
             <div>
+                <WarMenu />
                 <div className="grid-x">
                     <div className="cell">
                         <h1> Hello from React to Warframe </h1>
