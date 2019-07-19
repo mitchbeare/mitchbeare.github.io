68b597fc91eddd9e58dbf15dc74b92356abc09af
Mitchell Beare <Mitchellbeare@gmail.com>
2019-07-19 15:51:40 +1000

    Added checkbox callback

--- warframe/components/App.js
+++ warframe/components/App.js
@@ -1,6 +1,11 @@
-//import Menu from './menu.js';
-
 class App extends React.Component {
+  state = {};
+
+  saveCheck(e) {
+    e.preventDefault();
+    console.log(e, 'was clicked');
+  }
+
   render() {
     return (
       <div>
@@ -18,40 +23,56 @@ class App extends React.Component {
           <div className="cell-6">
             <ol>
               <li>
-                <input id="item1" type="checkbox" />
+                <input id="item1" type="checkbox" onClick={saveCheck} />
                 <label for="item1">Complete Vor's Prize Quest</label>
               </li>
               <li>
-                <input id="item2" type="checkbox" />
+                <input id="item2" type="checkbox" onClick={saveCheck} />
                 <label for="item2">Complete All missions on Earth</label>
               </li>
               <li>
-                <input id="item3" type="checkbox" />
+                <input id="item3" type="checkbox" onClick={saveCheck} />
                 <label for="item3">Farm Mods from Earth</label>
                 <ul>
                   <li>
-                    <input id="earthitem1" type="checkbox" />
+                    <input
+                      id="earthitem1"
+                      type="checkbox"
+                      onClick={saveCheck}
+                    />
                     <label for="earthitem1">
                       Ivara systems blue print from Cambria. (third vault 9.09%
                       chance)
                     </label>
                   </li>
                   <li>
-                    <input id="earthitem2" type="checkbox" />
+                    <input
+                      id="earthitem2"
+                      type="checkbox"
+                      onClick={saveCheck}
+                    />
                     <label for="earthitem2">
                       Farm Vicious Frost from Cambria. (third vault 9.09%
                       chance)
                     </label>
                   </li>
                   <li>
-                    <input id="earthitem3" type="checkbox" />
+                    <input
+                      id="earthitem3"
+                      type="checkbox"
+                      onClick={saveCheck}
+                    />
                     <label for="earthitem3">
                       Farm Volcanic edge from Cambria. (third vault 9.09%
                       chance)
                     </label>
                   </li>
                   <li>
-                    <input id="earthitem4" type="checkbox" />
+                    <input
+                      id="earthitem4"
+                      type="checkbox"
+                      onClick={saveCheck}
+                    />
                     <label for="earthitem4">
                       Farm Hornet strike from Cambria. (third vault 9.09%
                       chance)
@@ -64,7 +85,11 @@ class App extends React.Component {
                     </label>
                   </li>
                   <li>
-                    <input id="earthitem6" type="checkbox" />
+                    <input
+                      id="earthitem6"
+                      type="checkbox"
+                      onClick={saveCheck}
+                    />
                     <label for="earthitem6">
                       Farm Intensify from Everest. (third excavator 8.33%
                       chance)
@@ -73,7 +98,7 @@ class App extends React.Component {
                 </ul>
               </li>
               <li>
-                <input id="item4" type="checkbox" />
+                <input id="item4" type="checkbox" onClick={saveCheck} />
                 <label for="item4">
                   Unlock and complete the Venus junction
                 </label>
