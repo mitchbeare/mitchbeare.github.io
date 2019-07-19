5a88f20c816996bc9a18570f1d8d24342dcd22d5
Mitchell Beare <Mitchellbeare@gmail.com>
2019-07-19 13:08:11 +1000

    Added some menu components.

--- warframe/components/menu.js
+++ warframe/components/menu.js
@@ -2,9 +2,11 @@ class WarMenu extends React.Component{
     render(){
         return(
             <div id="menuarea">
-                <ul className="menu">
-                    <li><a>Item one</a></li>
-                    <li><a>Item two</a></li>
+                <ul className="menu expanded">
+                    <li className="menu-text">Warframe Reference</li>
+                    <li><a>Daily Tasks</a></li>
+                    <li><a>Placeholder 1</a></li>
+                    <li><a>Plaveholder 2</a></li>
                 </ul>
             </div>
         );
