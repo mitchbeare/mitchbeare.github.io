3f8d06f6c3290f15be955e05783fbea9b4257914
Mitchell Beare <Mitchellbeare@gmail.com>
2019-08-02 12:34:25 +1000

    Adjusted sizing of containers.

--- components/Splash.js
+++ components/Splash.js
@@ -2,14 +2,16 @@ class Splash extends React.Component {
   render() {
     return (
       <div className="flex mx-4 my-2">
-        <div className="w-1/3 bg-red-100">
+        <div className="w-1/3 bg-red-100 h-full">
           <h1>Mitchell Beare</h1>
         </div>
-        <div className="w-2/3 bg-green-100">
-          <h1>Work Experience</h1>
-        </div>
-        <div className="w-2/3 bg-blue-100">
-          <h1>Education</h1>
+        <div className="w-2/3">
+          <div className="bg-green-100">
+            <h1>Work Experience</h1>
+          </div>
+          <div className="bg-blue-100">
+            <h1>Education</h1>
+          </div>
         </div>
       </div>
     );
