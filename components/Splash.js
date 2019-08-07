29bc4a06697accf461edcee470e28ba1bd0d4b2c
Mitchell Beare <Mitchellbeare@gmail.com>
2019-08-07 14:42:31 +1000

    Made Social Media links live.

--- components/Splash.js
+++ components/Splash.js
@@ -17,9 +17,15 @@ class Splash extends React.Component {
         >
           <div className="w-full p-2 text-center">
             <p>Find me on social media</p>
-            <i className="fas fas-facebook-square" />
-            <i className="fas fas-linkedin" />
-            <i className="fas fas-github-square" />
+            <a href="https://www.facebook.com/mitchell.beare.1?ref=bookmarks">
+              <i className="fas fa-facebook-square" />
+            </a>
+            <a href="https://www.linkedin.com/in/mitchell-beare-0002b6151/">
+              <i className="fas fa-linkedin" />
+            </a>
+            <a href="https://github.com/mitchbeare">
+              <i className="fas fa-github-square" />
+            </a>
             <p>Powered by React with Tailwind CSS</p>
           </div>
         </div>
