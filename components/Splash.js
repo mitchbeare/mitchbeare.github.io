0355e8ecbc5b906f5930dc5953e0f8fea4a13a03
Mitchell Beare <Mitchellbeare@gmail.com>
2019-08-07 14:23:29 +1000

    Adjusted Footer images

--- components/Splash.js
+++ components/Splash.js
@@ -3,7 +3,7 @@ class Splash extends React.Component {
     return (
       <div>
         <div className="flex px-4 py-2 w-screen">
-          <div className="flex-auto w-1/3 bg-white rounded-lg shadow-xl m-2 h-full p-6">
+          <div className="flex-auto w-1/3 bg-white rounded-lg shadow-xl m-2 h-full">
             <AboutPanel />
           </div>
           <div className="flex-auto w-2/3 flex flex-col m-2">
@@ -11,12 +11,15 @@ class Splash extends React.Component {
             <EducationPanel />
           </div>
         </div>
-        <div className="w-full mx-auto" style={{ backgroundColor: '#16a085' }}>
+        <div
+          className="w-full mx-auto rounded-lg"
+          style={{ backgroundColor: '#16a085' }}
+        >
           <div className="w-full p-2 text-center">
             <p>Find me on social media</p>
-            <i className="fas facebook-square" />
-            <i className="fas linkedin" />
-            <i className="fas github-square" />
+            <i className="fas fas-facebook-square" />
+            <i className="fas fas-linkedin" />
+            <i className="fas fas-github-square" />
             <p>Powered by React with Tailwind CSS</p>
           </div>
         </div>
