b64d3091bc4b4c2577994e3ce7f7f57b63df244e
Mitchell Beare <Mitchellbeare@gmail.com>
2019-08-07 13:16:40 +1000

    Added margin to cards to prevent collision.

--- components/Splash.js
+++ components/Splash.js
@@ -3,10 +3,10 @@ class Splash extends React.Component {
     return (
       <div>
         <div className="flex px-4 py-2 w-screen">
-          <div className="flex-auto w-1/3 bg-white rounded-lg shadow-xl mx-auto h-full p-6">
+          <div className="flex-auto w-1/3 bg-white rounded-lg shadow-xl m-2 h-full p-6">
             <AboutPanel />
           </div>
-          <div className="flex-auto w-2/3 flex flex-col mx-auto">
+          <div className="flex-auto w-2/3 flex flex-col m-2">
             <WorkPanel />
             <EducationPanel />
           </div>
@@ -15,17 +15,19 @@ class Splash extends React.Component {
           className="w-full mx-auto"
           style={{ backgroundColor: '#27ae60' }}
         >
-          <div>
+          <div className="w-full p-2 text-center">
             <p>Find me on social media</p>
-            <a href="https://www.facebook.com/mitchell.beare.1">
-              <i className="fas facebook-square" />
-            </a>
-            <a href="https://www.linkedin.com/in/mitchell-beare-0002b6151/">
-              <i className="fas linkedin" />
-            </a>
-            <a href="https://github.com/mitchbeare">
-              <i className="fas github-square" />
-            </a>
+            <div className="flex">
+              <a href="https://www.facebook.com/mitchell.beare.1">
+                <i className="fas facebook-square" />
+              </a>
+              <a href="https://www.linkedin.com/in/mitchell-beare-0002b6151/">
+                <i className="fas linkedin" />
+              </a>
+              <a href="https://github.com/mitchbeare">
+                <i className="fas github-square" />
+              </a>
+            </div>
           </div>
         </footer>
       </div>
