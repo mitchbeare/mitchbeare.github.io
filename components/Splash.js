50fc8d6434d767b5209f97abe7780b3ea36054a2
Mitchell Beare <Mitchellbeare@gmail.com>
2019-08-07 15:53:38 +1000

    Massively increased margin

--- components/Splash.js
+++ components/Splash.js
@@ -2,7 +2,7 @@ class Splash extends React.Component {
   render() {
     return (
       <div>
-        <div className="flex px-4 py-2 w-screen sm:mx-1 md:mx-4 lg:mx-7 xl:mx-10">
+        <div className="flex px-4 py-2 w-screen sm:mx-40 md:mx-48 lg:mx-56 xl:mx-64">
           <div className="flex-auto w-2/5 bg-white rounded-lg shadow-xl m-2 h-full">
             <AboutPanel />
           </div>
