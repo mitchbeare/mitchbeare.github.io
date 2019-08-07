ea9a1ea95d07210e15fbc48808a4fe80664877f1
Mitchell Beare <Mitchellbeare@gmail.com>
2019-08-07 10:49:27 +1000

    Added color to skill bar, pull in prop for subheading.

--- components/SkillBar.js
+++ components/SkillBar.js
@@ -6,12 +6,15 @@ class SkillBar extends React.Component {
 
   render() {
     return (
-      <div className="shadow w-full bg-grey-light mt-2">
-        <div
-          className="bg-blue text-xs leading-none py-1 text-center text-white"
-          style={{ width: '45%' }}
-        >
-          45%
+      <div>
+        <h2>{this.props.skillName}</h2>
+        <div className="shadow w-full bg-grey-light mt-2">
+          <div
+            className="bg-blue text-xs leading-none py-1 text-center text-white"
+            style={{ width: '45%', backgroundColor: 'blue' }}
+          >
+            45%
+          </div>
         </div>
       </div>
     );
