6b7875da914fd0c7045d17a8c4da3d63b153adb4
Mitchell Beare <Mitchellbeare@gmail.com>
2019-09-05 10:00:14 +1000

    Fixed broken icons in footer.

--- components/Splash.js
+++ components/Splash.js
@@ -14,14 +14,20 @@ class Splash extends React.Component {
         <div className="w-full mx-auto" style={{ backgroundColor: '#16a085' }}>
           <div className="w-full p-2 text-center">
             <p>Find me on social media</p>
-            <a href="https://www.facebook.com/mitchell.beare.1?ref=bookmarks">
-              <i className="fas fa-facebook" />
+            <a
+              className="mx-1"
+              href="https://www.facebook.com/mitchell.beare.1?ref=bookmarks"
+            >
+              <i className="fab fa-facebook" />
             </a>
-            <a href="https://www.linkedin.com/in/mitchell-beare-0002b6151/">
-              <i className="fas fa-linkedin" />
+            <a
+              className="mx-1"
+              href="https://www.linkedin.com/in/mitchell-beare-0002b6151/"
+            >
+              <i className="fab fa-linkedin" />
             </a>
-            <a href="https://github.com/mitchbeare">
-              <i className="fas fa-github" />
+            <a className="mx-1" href="https://github.com/mitchbeare">
+              <i className="fab fa-github" />
             </a>
             <p>
               Powered by <a href="https://reactjs.org/">React</a> with{' '}
